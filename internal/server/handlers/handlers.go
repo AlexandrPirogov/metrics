0976fb07c11a512eba9af303ee862096182c385e
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-03-12 20:52:40 +0300

    added handler for Get router

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -11,6 +11,18 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+// GetDocuments returns saved metrics on server
+func GetDocuments(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(db.Read()))
+}
+
+// UpdateHandler saves incoming metrics
+//
+// Pre-cond: given correct type, name and val of metrics
+//
+// Post-cond: correct metrics saved on server
 func UpdateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	mtype := chi.URLParam(r, "mtype")
@@ -32,6 +44,11 @@ func UpdateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// isUpdatePathCorrect if given metric is correct
+//
+// Pre-cond: given string type name and val
+//
+// Post-cond: if metric is correct -- return 0. Otherwise -1
 func isUpdatePathCorrect(mtype, mname, mval string) int {
 	var gauges = metrics.MemStats{}
 	var counters = metrics.Polls{}
@@ -43,7 +60,7 @@ func isUpdatePathCorrect(mtype, mname, mval string) int {
 	// If given incorrect path
 	if _, ok := mTypes[mtype]; !ok {
 		log.Printf("Given metric type %s not exists!", mtype)
-		return 1
+		return -1
 	}
 
 	if metricsCheck(&gauges, mname, mval) == 0 || metricsCheck(&counters, mname, mval) == 0 {
@@ -52,6 +69,11 @@ func isUpdatePathCorrect(mtype, mname, mval string) int {
 	return -1
 }
 
+// metricsCheck checks if given name is correct for given type of metric
+//
+// Pre-cond: given string type and val
+//
+// Post-cond: if metric is correct -- return 0. Otherwise -1
 func metricsCheck(metric metrics.Metricable, metricName string, metricVal string) int {
 	var metrics = make(map[string]bool)
 	counterVal := reflect.TypeOf(metric).Elem()
@@ -61,12 +83,12 @@ func metricsCheck(metric metrics.Metricable, metricName string, metricVal string
 
 	if _, ok := metrics[metricName]; !ok {
 		log.Printf("Metric %s isn't exists!\n", metricName)
-		return 1
+		return -1
 	}
 
 	if _, err := strconv.ParseFloat(metricVal, 64); err != nil {
 		log.Printf("Error: %v", err)
-		return 2
+		return -1
 	}
 	return 0
 }
