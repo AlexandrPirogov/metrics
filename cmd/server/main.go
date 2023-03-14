a496eeb8c78801295d9c2f53806e1fff5dc02f03
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-03-14 13:01:30 +0300

    added listening OS signals and catching them

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"log"
 	"memtracker/internal/server/handlers"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Post("/update/{mtype}/{mname}/{val}", handlers.UpdateHandler)
-	r.Get("/value/{mtype}/{mname}", handlers.RetrieveMetric)
-	r.Get("/", handlers.RetrieveMetrics)
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-	server.ListenAndServe()
+	cancelChan := make(chan os.Signal, 1)
+
+	signal.Notify(cancelChan, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		r := chi.NewRouter()
+		r.Use(middleware.Logger)
+		r.Post("/update/{mtype}/{mname}/{val}", handlers.UpdateHandler)
+		r.Get("/value/{mtype}/{mname}", handlers.RetrieveMetric)
+		r.Get("/", handlers.RetrieveMetrics)
+		server := &http.Server{
+			Addr:    ":8080",
+			Handler: r,
+		}
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Printf("%v", err)
+		}
+	}()
+	sig := <-cancelChan
+	log.Printf("Caught signal %v", sig)
+	close(cancelChan)
 }
