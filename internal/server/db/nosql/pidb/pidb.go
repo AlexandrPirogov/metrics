8dc437da5bb8a1699d4ae565a769f7e5f15456bd
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-03-15 19:23:27 +0300

    replcaed mutex

--- internal/server/db/nosql/pidb/pidb.go
+++ internal/server/db/nosql/pidb/pidb.go
@@ -66,8 +66,6 @@ func (p *MemStorage) Metrics() string {
 // Post-condition: insert opertaion executed.
 // Returns 0 if successed. Otherwise means fail
 func (p *MemStorage) InsertMetric(mtype, name, val string) error {
-	p.Mutex.Lock()
-	defer p.Mutex.Unlock()
 	log.Printf("Got--%s--%s--", name, mtype)
 	if metrics.IsMetricCorrect(mtype, name) != nil {
 		errMsg := fmt.Sprintf("Given not existing metric %s %s\n", mtype, name)
@@ -85,6 +83,8 @@ func (p *MemStorage) InsertMetric(mtype, name, val string) error {
 // Post-condition: insert opertaion executed.
 // Returns 0 if successed. Otherwise means fail
 func (p *MemStorage) insertJSON(mtype, name string, val string) {
+	p.Mutex.Lock()
+	defer p.Mutex.Unlock()
 	document := p.newJSON(mtype, name, val)
 	p.Documents[mtype][name] = document
 }
