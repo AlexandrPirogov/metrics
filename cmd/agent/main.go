27deff70468c99f87256d1d2a6610349102ea93d
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-03-19 15:03:35 +0300

    added OS signal catching in agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
 	"memtracker/internal/memtrack"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -11,7 +15,22 @@ var host string = "localhost"
 var port string = ":8080"
 
 func main() {
-	var client = http.Client{Timeout: time.Second / 2}
-	memtracker := memtrack.NewHTTPMemTracker(client, host+port)
-	memtracker.ReadAndSend(time.Second*2, time.Second*10)
+	go func() {
+		var client = http.Client{Timeout: time.Second / 2}
+		memtracker := memtrack.NewHTTPMemTracker(client, host+port)
+		memtracker.ReadAndSend(time.Second*2, time.Second*10)
+
+	}()
+
+	cancelChan := make(chan os.Signal, 1)
+	signal.Notify(cancelChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT)
+
+	<-cancelChan
+	log.Printf("os.Interrupt-- shutting down...\n")
+	go func() {
+		<-cancelChan
+		log.Fatalf("os.Kill -- terminating...\n")
+	}()
+	defer os.Exit(0)
+	close(cancelChan)
 }
