81926874430524b1417ad9b8afe90aefa6d701df
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-01 14:41:26 +0300

    added flags for application

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,17 @@ package config
 
 type Interval string
 
+// flags
+var (
+	Address       string // agent & server addr
+	Restore       bool   // Should db be restored
+	StoreInterval string // period of replication
+	StoreFile     string // file where replication is goint to be written
+
+	ReportInterval string // how often agent will sends metrics to server
+	PollInterval   string // how often agent will updates metrics
+)
+
 type ClientConfig struct {
 	Address        string   `env:"ADDRESS" envDefault:"localhost:8080"`
 	ReportInterval Interval `env:"REPORT_INTERVAL" envDefault:"10s"`
@@ -16,4 +27,8 @@ type JournalConfig struct {
 	StoreFile    string `env:"STORE_FILE"`
 	Restore      bool   `env:"RESTORE" envDefault:"true"`
 	ReadInterval string `env:"STORE_INTERVAL" envDefault:"300"`
+}
+
+func init() {
+
 }
