22da1ce3635d17b138458c5a0ce775680e0c74c7
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-03 11:44:30 +0300

    added comments

--- internal/server/handlers/api/updater.go
+++ internal/server/handlers/api/updater.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+// processUpdate updates metric value depends on metric's type
+//
+// Pre-cond: given metric
+//
+// Post-cond: return result or processing metric.
+// If success, returns slice of bytes and http status = 200
+// otherwise returns empty bite slice and corresponging http status
 func (d *DefaultHandler) processUpdate(metric metrics.Metrics) ([]byte, int) {
 	switch {
 	case metric.MType == "gauge":
@@ -17,6 +24,13 @@ func (d *DefaultHandler) processUpdate(metric metrics.Metrics) ([]byte, int) {
 	}
 }
 
+// processUpdateCounter updates counter metric
+//
+// Pre-cond: given counter metric
+//
+// Post-cond: return result or processing counter  metric.
+// If success, returns slice of bytes and http status = 200
+// otherwise returns empty bite slice and corresponging http status
 func (d *DefaultHandler) processUpdateCounter(metric metrics.Metrics) ([]byte, int) {
 	if metric.Delta == nil || metric.Value != nil {
 		return []byte{}, http.StatusBadRequest
@@ -27,13 +41,18 @@ func (d *DefaultHandler) processUpdateCounter(metric metrics.Metrics) ([]byte, i
 	}
 }
 
+// processUpdateCounter updates update metric
+//
+// Pre-cond: given update metric
+//
+// Post-cond: return result or processing update  metric.
+// If success, returns slice of bytes and http status = 200
+// otherwise returns empty bite slice and corresponging http status
 func (d *DefaultHandler) processUpdateGauge(metric metrics.Metrics) ([]byte, int) {
 	if metric.Value == nil || metric.Delta != nil {
 		return []byte{}, http.StatusBadRequest
 	} else {
-		body, err := d.DB.Write(metric.MType, metric.ID, fmt.Sprintf("%.11f", *metric.Value))
-		if err != nil {
-		}
+		body, _ := d.DB.Write(metric.MType, metric.ID, fmt.Sprintf("%.11f", *metric.Value))
 		return body, http.StatusOK
 	}
 }
