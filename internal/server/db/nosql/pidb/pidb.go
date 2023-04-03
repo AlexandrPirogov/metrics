8a14f59bb031e4efef703e8b9524ce6b340454c4
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-03 12:54:49 +0300

    added comment to pidb and made code a bit cleaner

--- internal/server/db/nosql/pidb/pidb.go
+++ internal/server/db/nosql/pidb/pidb.go
@@ -83,6 +83,30 @@ func (p *MemStorage) InsertMetric(mtype, name, val string) ([]byte, error) {
 	return bytes, err
 }
 
+// Creates Metric by given args and insert it to storage
+//
+// Pre-cond: given correct args for Metrics
+//
+// Post-condition: insert opertaion executed.
+// Returns 0 if successed. Otherwise means fail
+func (p *MemStorage) insertJSON(mtype, name string, val string) ([]byte, error) {
+	p.Mutex.Lock()
+	defer p.Mutex.Unlock()
+	document, err := p.newJSON(mtype, name, val)
+	if err != nil {
+		return []byte{}, err
+	}
+	p.Metrics[mtype][name] = document
+	return document, nil
+}
+
+// RestoreMetric store metrics that was restored (read from file for example)
+//
+// Pre-cond: given correct type, non-empty name and correct val for corresponding metric
+//
+// Post-cond: saved marshaled metric in DB.
+// If Success then returns slice of bytes that saved in DB and nil error
+// Otherwise returns empty slice of bytes and errors
 func (p *MemStorage) RestoreMetric(mtype, name, val string) ([]byte, error) {
 	if metrics.IsMetricCorrect(mtype, name) != nil {
 		errMsg := fmt.Sprintf("given not existing metric %s %s\n", mtype, name)
@@ -96,16 +120,17 @@ func (p *MemStorage) RestoreMetric(mtype, name, val string) ([]byte, error) {
 	return bytes, err
 }
 
-// Creates Metric by given args and insert it to storage
+// restoreJSON creates metric from given params, marshal it and saves it to DB
 //
-// Pre-cond: given correct args for Metrics
+// Pre-cond: given correct type, non-empty name and correct val for corresponding metric
 //
-// Post-condition: insert opertaion executed.
-// Returns 0 if successed. Otherwise means fail
-func (p *MemStorage) insertJSON(mtype, name string, val string) ([]byte, error) {
+// Post-cond: saved marshaled metric in DB.
+// If Success then returns slice of bytes that saved in DB and nil error
+// Otherwise returns empty slice of bytes and errors
+func (p *MemStorage) restoreJSON(mtype, name string, val string) ([]byte, error) {
 	p.Mutex.Lock()
 	defer p.Mutex.Unlock()
-	document, err := p.newJSON(mtype, name, val, false)
+	document, err := p.newRestoredJSON(mtype, name, val, true)
 	if err != nil {
 		return []byte{}, err
 	}
@@ -113,65 +138,84 @@ func (p *MemStorage) insertJSON(mtype, name string, val string) ([]byte, error) 
 	return document, nil
 }
 
-func (p *MemStorage) restoreJSON(mtype, name string, val string) ([]byte, error) {
-	p.Mutex.Lock()
-	defer p.Mutex.Unlock()
-	document, err := p.newJSON(mtype, name, val, true)
-	if err != nil {
-		return []byte{}, err
+// newJSON creates json from given params
+//
+// Pre-cond: given mtype, non-empty name and corresponding val of metric.
+// mtype and name should be one of from package metrics.
+//
+// Post-condition: creats new Metric instance or returns error
+func (p *MemStorage) newJSON(mtype, name, val string) ([]byte, error) {
+	if mtype == "counter" {
+		return p.counterJSON(mtype, name, val)
 	}
-	p.Metrics[mtype][name] = document
-	return document, nil
+	return p.gagueJSON(mtype, name, val)
 }
 
 // Pre-cond: given mtype, name and val of metric.
 // mtype and name should be one of from package metrics.
 //
 // Post-condition: creats new Metric instance or returns error
-func (p *MemStorage) newJSON(mtype, name, val string, shouldRestore bool) ([]byte, error) {
+func (p *MemStorage) newRestoredJSON(mtype, name, val string, shouldRestore bool) ([]byte, error) {
 	if mtype == "counter" {
-		return p.counterJSON(mtype, name, val, shouldRestore)
+		return p.restoredCounterJSON(mtype, name, val)
 	}
 	return p.gagueJSON(mtype, name, val)
 }
 
-// counterJSON converts gauge to JSON
-func (p *MemStorage) counterJSON(mtype, name, val string, shouldRestore bool) ([]byte, error) {
+// restoredCounterJSON creates metric by given params and marshal it
+//
+// Pre-cond: given correect params for counter metric
+//
+// Post-cond: metric was created with given params and marshaled
+// If success then return slice of bytes and nil error
+// Otherwise returns empty slice of bytes and error
+func (p *MemStorage) restoredCounterJSON(mtype, name, val string) ([]byte, error) {
 	var toUpdate metrics.Metrics
-	if doc, ok := p.Metrics[mtype][name]; ok {
+	toAdd, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return []byte{}, err
+	}
+	toUpdate.Delta = &toAdd
+	toUpdate.ID = name
+	toUpdate.MType = mtype
+	return json.Marshal(toUpdate)
+}
 
+// counterJSON create counter metric from given params and marshal it to json
+//
+// Pre-cond: given correct ptype, non-empty name and val for counter metric
+//
+// Post-cond: created metric and marshaled to json. If metric was existed in DB
+// then Delta increased by given val
+func (p *MemStorage) counterJSON(mtype, name, val string) ([]byte, error) {
+	toAdd, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	toUpdate := metrics.Metrics{ID: name, MType: mtype}
+	if doc, ok := p.Metrics[mtype][name]; ok {
 		err := json.Unmarshal(doc, &toUpdate)
 		if err != nil {
 			return []byte{}, err
 		}
-		toAdd, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			return []byte{}, err
-		}
-		if !shouldRestore {
-			delta := *toUpdate.Delta + toAdd
-			toUpdate.Delta = &delta
-		} else {
-			toUpdate.Delta = &toAdd
-		}
+		delta := *toUpdate.Delta + toAdd
+		toUpdate.Delta = &delta
 
 	} else {
-		toUpdate = metrics.Metrics{}
-		toAdd, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			return []byte{}, err
-		}
 		toUpdate.Delta = &toAdd
 	}
-
-	toUpdate.ID = name
-	toUpdate.MType = mtype
-
 	return json.Marshal(toUpdate)
 
 }
 
 // gagueJSON converts gauge to JSON
+//
+// Pre-cond: given correct params for gauge metric
+//
+// Post-cond: created metric by given params and marshaled.
+// If success return slice of bytes and nil error
+// Otherwise return empty slice of bytes and error
 func (p *MemStorage) gagueJSON(mtype, name, val string) ([]byte, error) {
 	toInsert := metrics.Metrics{
 		ID:    name,
