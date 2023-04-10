17fae0e9061b6f4a28da8f0c9865172f20c5d933
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-10 13:07:57 +0300

    added key as param for hash method

--- internal/crypt/crypt.go
+++ internal/crypt/crypt.go
@@ -6,12 +6,11 @@ import (
 	"memtracker/internal/config/agent"
 )
 
-func Hash(toHash string) string {
+func Hash(toHash, key string) string {
 	cfg := agent.ClientCfg
 	if cfg.Hash == "" {
 		return ""
 	}
-	key := cfg.Hash
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(toHash))
 	return string(h.Sum(nil))
