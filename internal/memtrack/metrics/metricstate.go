182df64f2d24e27271affab4cb73abbd264a4555
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-13 14:34:23 +0300

    added function for convertion counter state to tuple

--- internal/memtrack/metrics/metricstate.go
+++ internal/memtrack/metrics/metricstate.go
@@ -1,15 +1,22 @@
 package metrics
 
-type StateTupler interface {
-	SetField(key string, value interface{})
-	GetField(Key string) interface{}
-}
+import "memtracker/internal/kernel/tuples"
 
 // Struct to represent Counter metrics as tuple in FP style
 type CounterState struct {
 	Name  string   `json:"id"`              //Metric name
 	Type  string   `json:"type"`            // Metric type: gauge or counter
 	Value *counter `json:"delta,omitempty"` //Metric's val if passing counter
+}
+
+func (c CounterState) ToTuple() tuples.Tuple {
+	tuple := tuples.Tuple{
+		Fields: make(map[string]interface{}),
+	}
+	tuple.SetField("name", c.Name)
+	tuple.SetField("type", c.Type)
+	tuple.SetField("value", c.Value)
+	return tuple
 }
 
 // Struct to represent Counter metrics as tuple in FP style
