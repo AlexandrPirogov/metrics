e95120070e73ba9f444902508a9d1d9eec64bed8
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-13 14:09:17 +0300

    added comms for structs

--- internal/memtrack/metrics/metricstate.go
+++ internal/memtrack/metrics/metricstate.go
@@ -5,12 +5,14 @@ type StateTupler interface {
 	GetField(Key string) interface{}
 }
 
+// Struct to represent Counter metrics as tuple in FP style
 type CounterState struct {
 	Name  string   `json:"id"`              //Metric name
 	Type  string   `json:"type"`            // Metric type: gauge or counter
 	Value *counter `json:"delta,omitempty"` //Metric's val if passing counter
 }
 
+// Struct to represent Counter metrics as tuple in FP style
 type GaugeState struct {
 	Name  string `json:"id"`              //Metric name
 	Type  string `json:"type"`            // Metric type: gauge or counter
