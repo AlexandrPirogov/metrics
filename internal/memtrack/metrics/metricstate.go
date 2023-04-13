86bf45a93a1edea39071a639bc523d5808409e38
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-13 14:36:51 +0300

    added func for GaugeState to convert to tuple

--- internal/memtrack/metrics/metricstate.go
+++ internal/memtrack/metrics/metricstate.go
@@ -9,6 +9,11 @@ type CounterState struct {
 	Value *counter `json:"delta,omitempty"` //Metric's val if passing counter
 }
 
+// ToTuple convers CounterState to tuple.Tuple
+//
+// Pre-cond:
+//
+// Post-cond: tuples instance are returned
 func (c CounterState) ToTuple() tuples.Tuple {
 	tuple := tuples.Tuple{
 		Fields: make(map[string]interface{}),
@@ -19,9 +24,24 @@ func (c CounterState) ToTuple() tuples.Tuple {
 	return tuple
 }
 
-// Struct to represent Counter metrics as tuple in FP style
+// Struct to represent gauge metrics as tuple in FP style
 type GaugeState struct {
 	Name  string `json:"id"`              //Metric name
 	Type  string `json:"type"`            // Metric type: gauge or counter
 	Value *gauge `json:"value,omitempty"` //Metric's val if passing counter
+}
+
+// ToTuple convers GaugeState to tuple.Tuple
+//
+// Pre-cond:
+//
+// Post-cond: tuples instance are returned
+func (g GaugeState) ToTuple() tuples.Tuple {
+	tuple := tuples.Tuple{
+		Fields: make(map[string]interface{}),
+	}
+	tuple.SetField("name", g.Name)
+	tuple.SetField("type", g.Type)
+	tuple.SetField("value", g.Value)
+	return tuple
 }
