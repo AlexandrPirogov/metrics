46b2c5feee1526f93c58df4dba7a54eeb7907fe6
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-13 14:07:46 +0300

    added test forgauge marsha

--- internal/memtrack/metrics/metricstate_test.go
+++ internal/memtrack/metrics/metricstate_test.go
@@ -28,3 +28,25 @@ func TestCorrectCreateCounterMetricStateFromCounterMetric(t *testing.T) {
 	assert.EqualValues(t, metric.MType, sut.Type)
 	assert.EqualValues(t, metric.Delta, sut.Value)
 }
+
+func TestCorrectCreateGaugeMetricStateFromGaugeMetric(t *testing.T) {
+	//Arrange
+	val := float64(1.11111111)
+	metric := Metrics{
+		ID:    "qwe",
+		MType: "gauge",
+		Value: &val,
+	}
+
+	body, _ := json.Marshal(metric)
+	var sut GaugeState
+
+	//Act
+
+	err := json.Unmarshal(body, &sut)
+	//Assert
+	assert.Nil(t, err)
+	assert.EqualValues(t, metric.ID, sut.Name)
+	assert.EqualValues(t, metric.MType, sut.Type)
+	assert.EqualValues(t, metric.Value, sut.Value)
+}
