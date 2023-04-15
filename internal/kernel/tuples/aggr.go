b4881d2626414f87475c016fffa0dc3707b2afe9
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-15 16:33:51 +0300

    removed unused function

--- internal/kernel/tuples/aggr.go
+++
@@ -1,9 +0,0 @@
-package tuples
-
-func Append[T int64 | float64](f string, t, t1 Tupler) Tupler {
-	//TODO
-	f1, _ := t.GetField(f)
-	f2, _ := t1.GetField(f)
-	t.SetField(f, f1.(T)+f2.(T))
-	return t
-}
