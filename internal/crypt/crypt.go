1b2d7c8d39222589ace0bf51395ad453f78f40e5
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-18 13:23:32 +0300

    removed log

--- internal/crypt/crypt.go
+++ internal/crypt/crypt.go
@@ -4,7 +4,6 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
-	"log"
 )
 
 func Hash(toHash, key string) string {
@@ -14,6 +13,5 @@ func Hash(toHash, key string) string {
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(toHash))
 	res := h.Sum(nil)
-	log.Printf("%x", res)
 	return fmt.Sprintf("%x", res)
 }
