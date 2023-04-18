7c17625d8f416ebd34bf0115e66258e21148adc0
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-04-18 16:29:50 +0300

    added gzip and butch sending to client

--- internal/memtrack/http/client/client.go
+++ internal/memtrack/http/client/client.go
@@ -28,8 +28,79 @@ type Client struct {
 
 func (c Client) SendCounter(metric metrics.Metricable, mapMetrics map[string]interface{}) {
 	log.Printf("sending to host: %s", c.Host)
+	toMarshal := c.BuildCounters(metric, mapMetrics)
+	url := "http://" + c.Host + "/updates/"
+
+	js, err := json.Marshal(toMarshal)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+
+	buffer := bytes.NewBuffer(js)
+	request, _ := http.NewRequest(http.MethodPost, url, buffer)
+	request.Header.Add("Content-Encoding", "gzip")
+	resp, err := c.Client.Do(request)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("error while readall %v", err)
+	}
+
+}
+
+func (c Client) SendGauges(metric metrics.Metricable, mapMetrics map[string]interface{}) {
+	toMarshal := c.BuildGauges(metric, mapMetrics)
+	url := "http://" + c.Host + "/updates/"
+
+	js, err := json.Marshal(toMarshal)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+
+	buffer := bytes.NewBuffer(js)
+	log.Printf("sending to host: %s", buffer)
+	request, _ := http.NewRequest(http.MethodPost, url, buffer)
+	request.Header.Add("Content-Encoding", "gzip")
+	resp, err := c.Client.Do(request)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("error while readall %v", err)
+	}
+}
+
+func (c Client) BuildGauges(metric metrics.Metricable, gauges map[string]interface{}) []metrics.Metrics {
 	key := agent.ClientCfg.Hash
-	for k, v := range mapMetrics {
+	res := make([]metrics.Metrics, 0)
+	for k, v := range gauges {
+		val := v.(float64)
+		toMarsal := metrics.Metrics{
+			ID:    k,
+			MType: metric.String(),
+			Value: &val,
+			Hash:  crypt.Hash(fmt.Sprintf("%s:gauge:%f", k, val), key),
+		}
+		res = append(res, toMarsal)
+	}
+	return res
+}
+
+func (c Client) BuildCounters(metric metrics.Metricable, counter map[string]interface{}) []metrics.Metrics {
+	key := agent.ClientCfg.Hash
+	res := make([]metrics.Metrics, 0)
+	for k, v := range counter {
 		val := v.(float64)
 		del := int64(val)
 		toMarsal := metrics.Metrics{
@@ -38,59 +109,7 @@ func (c Client) SendCounter(metric metrics.Metricable, mapMetrics map[string]int
 			Delta: &del,
 			Hash:  crypt.Hash(fmt.Sprintf("%s:counter:%d", k, del), key),
 		}
-		url := "http://" + c.Host + "/update/"
-
-		js, err := json.Marshal(toMarsal)
-		if err != nil {
-			log.Printf("%v", err)
-			continue
-		}
-		buffer := bytes.NewBuffer(js)
-		resp, err := c.Client.Post(url, c.ContentType, buffer)
-
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		_, err = io.ReadAll(resp.Body)
-		if err != nil {
-			log.Printf("error while readall %v", err)
-		}
+		res = append(res, toMarsal)
 	}
-}
-
-func (c Client) SendGauges(metric metrics.Metricable, mapMetrics map[string]interface{}) {
-	log.Printf("sending to host: %s", c.Host)
-	key := agent.ClientCfg.Hash
-	for k, v := range mapMetrics {
-		val := v.(float64)
-		toMarsal := metrics.Metrics{
-			ID:    k,
-			MType: metric.String(),
-			Value: &val,
-			Hash:  crypt.Hash(fmt.Sprintf("%s:gauge:%f", k, val), key),
-		}
-		url := "http://" + c.Host + "/update/"
-
-		js, err := json.Marshal(toMarsal)
-		if err != nil {
-			log.Printf("%v", err)
-			continue
-		}
-		buffer := bytes.NewBuffer(js)
-		resp, err := c.Client.Post(url, c.ContentType, buffer)
-
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		_, err = io.ReadAll(resp.Body)
-		if err != nil {
-			log.Printf("error while readall %v", err)
-		}
-	}
+	return res
 }
