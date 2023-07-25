4646b5a3c9738e763c2dcd836ed89228c5efbaa4
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-07-25 14:10:53 +0300

    code style

--- internal/config/agent/agent.go
+++ internal/config/agent/agent.go
@@ -3,9 +3,12 @@ package agent
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"io/ioutil"
 	"log"
+	f "memtracker/internal/function"
 	"net/http"
+	"os"
 
 	"github.com/caarlos0/env/v7"
 	"github.com/spf13/cobra"
@@ -26,10 +29,12 @@ var (
 // flags
 var (
 	address        string // agent & server addr
+	cfgFile        string // path to config json file
 	reportInterval string // how often agent will sends metrics to server
 	pollInterval   string // how often agent will updates metrics
 	hash           string //hash for metric
 	limit          int    //rate limit for agent to send requests
+
 )
 
 // Configs
@@ -38,53 +43,18 @@ var (
 )
 
 type ClientConfig struct {
-	Address        string   `env:"ADDRESS" envDefault:"localhost:8080"`
-	ReportInterval Interval `env:"REPORT_INTERVAL" envDefault:"10s"`
-	PollInterval   Interval `env:"POLL_INTERVAL" envDefault:"2s"`
+	Address        string   `env:"ADDRESS" envDefault:"localhost:8080" json:"address"`
+	ReportInterval Interval `env:"REPORT_INTERVAL" envDefault:"10s" json:"report_interval"`
+	PollInterval   Interval `env:"POLL_INTERVAL" envDefault:"2s" json:"poll_interval"`
 	Hash           string   `env:"KEY"`
 	Limit          int      `env:"RATE_LIMIT" envDefault:"1"`
-	CryptoKey      string   `env:"CRYPTO_KEY"`
+	CryptoKey      string   `env:"CRYPTO_KEY" json:"crypto_key"`
 	TransportCfg   *http.Transport
 }
 
-func Exec() {
-	initEnv()
-	initFlags()
-
-}
-
-func initEnv() {
-	if err := env.Parse(&ClientCfg); err != nil {
-		log.Fatalf("error while read client env variables %v", err)
-	}
-}
-
-func initFlags() {
-
-	rootClientCmd.PersistentFlags().StringVarP(&address, "address", "a", "", "ADDRESS OF AGNET. Default value: localhost:8080")
-	rootClientCmd.PersistentFlags().StringVarP(&reportInterval, "report", "r", "", "How ofter sends metrics to server. Examples: 0s, 10s, 100s")
-	rootClientCmd.PersistentFlags().StringVarP(&pollInterval, "poll", "p", "", "How often metrics are updates. Examples: 0s, 10s, 100s")
-	rootClientCmd.PersistentFlags().StringVarP(&hash, "key", "k", "", "key for encrypt data that's passes to server")
-	rootClientCmd.PersistentFlags().IntVarP(&limit, "limit", "l", 1, "rps limit to send requests")
-
-	if err := rootClientCmd.Execute(); err != nil {
-		log.Fatalf("%v", err)
-	}
-	if address != "" {
-		ClientCfg.Address = address
-	}
-
-	if hash != "" {
-		ClientCfg.Hash = hash
-	}
-
-	if limit != 1 {
-		ClientCfg.Limit = limit
-	}
-
-	if ClientCfg.CryptoKey == "" {
-		ClientCfg.TransportCfg = &http.Transport{}
-	} else {
+var (
+	assignNonTLS = func() { ClientCfg.TransportCfg = &http.Transport{} }
+	assignTLS    = func() {
 		ClientCfg.TransportCfg = &http.Transport{
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: true,
@@ -94,24 +64,63 @@ func initFlags() {
 			},
 		}
 	}
+)
+
+func Exec() {
+	initEnv()
+	initFlags()
 
 }
 
+func initEnv() {
+	err := env.Parse(&ClientCfg)
+	f.ErrFatalCheck("error while read client env variables", err)
+}
+
+func initFlags() {
+
+	rootClientCmd.PersistentFlags().StringVarP(&address, "address", "a", "", "ADDRESS OF AGNET. Default value: localhost:8080")
+	rootClientCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "PATH TO CONFIG FILE")
+	rootClientCmd.PersistentFlags().StringVarP(&reportInterval, "report", "r", "", "How ofter sends metrics to server. Examples: 0s, 10s, 100s")
+	rootClientCmd.PersistentFlags().StringVarP(&pollInterval, "poll", "p", "", "How often metrics are updates. Examples: 0s, 10s, 100s")
+	rootClientCmd.PersistentFlags().StringVarP(&hash, "key", "k", "", "key for encrypt data that's passes to server")
+	rootClientCmd.PersistentFlags().IntVarP(&limit, "limit", "l", 1, "rps limit to send requests")
+
+	err := rootClientCmd.Execute()
+	f.ErrFatalCheck("", err)
+
+	f.CompareStringsDo(cfgFile, "", func() { readConfigFile(cfgFile) })
+	f.CompareStringsDo(address, "", func() { ClientCfg.Address = address })
+	f.CompareStringsDo(hash, "", func() { ClientCfg.Hash = hash })
+	f.CompareStringsDo(ClientCfg.CryptoKey, "", func() {})
+	f.CompareIntsDo(limit, 1, func() { ClientCfg.Limit = limit })
+	f.CompareStringsDoOthewise(cfgFile, "", assignTLS, assignNonTLS)
+}
+
 func certTemplate(clientKet string) tls.Certificate {
-	crt, err := tls.LoadX509KeyPair("client.pem", "client.key")
-	if err != nil {
-		log.Fatalf("err while loading x509 key pair: %v", err)
-	}
+	crt, err := tls.LoadX509KeyPair("client.pem", clientKet)
+	f.ErrFatalCheck("err while loading x509 key pair", err)
+
 	certPool, err := x509.SystemCertPool()
-	if err != nil {
-		log.Fatalf("system certpool %v", err)
-	}
-	caCertPem, err := ioutil.ReadFile("cert.pem")
-	if err != nil {
-		log.Fatalf("err while reading cert.pem %v", err)
-	}
+	f.ErrFatalCheck("system certpool", err)
+
+	caCertPem, err := ioutil.ReadFile("client.pem")
+	f.ErrFatalCheck("err while reading client.pem", err)
+
 	if ok := certPool.AppendCertsFromPEM(caCertPem); !ok {
 		log.Fatal("invalid cert in CA PEM")
 	}
+
 	return crt
+}
+
+func readConfigFile(path string) {
+	bytes, err := os.ReadFile(path)
+	f.ErrFatalCheck("err while reading config json file", err)
+
+	log.Printf("found agent config file %s", path)
+	err = json.Unmarshal(bytes, &ClientCfg)
+	f.ErrFatalCheck("", err)
+
+	log.Printf("applied agent config file %s", path)
 }
