e84caae043bdce9eec4780186f7eae1208fa9be1
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-07-25 14:10:42 +0300

    added functino for int compare

--- internal/function/function.go
+++ internal/function/function.go
@@ -3,15 +3,15 @@ package function
 import "log"
 
 // Helper function-values for applying
-type assignFunction func()
+type onSuccessFunction func()
 
-func CompareStringsDo(check string, defaulValue string, actionOnTrue assignFunction) {
+func CompareStringsDo(check string, defaulValue string, actionOnTrue onSuccessFunction) {
 	if check != defaulValue {
 		actionOnTrue()
 	}
 }
 
-func CompareStringsDoOthewise(check string, defaulValue string, actionOnTrue assignFunction, actionOnFalse assignFunction) {
+func CompareStringsDoOthewise(check string, defaulValue string, actionOnTrue onSuccessFunction, actionOnFalse onSuccessFunction) {
 	if check != defaulValue {
 		actionOnTrue()
 		return
@@ -20,8 +20,14 @@ func CompareStringsDoOthewise(check string, defaulValue string, actionOnTrue ass
 	actionOnFalse()
 }
 
-func CompareBoolssDo(check bool, defaulValue bool, actionOnTrue assignFunction) {
+func CompareBoolssDo(check bool, defaulValue bool, actionOnTrue onSuccessFunction) {
 	if check != defaulValue {
+		actionOnTrue()
+	}
+}
+
+func CompareIntsDo(check int, with int, actionOnTrue onSuccessFunction) {
+	if check != with {
 		actionOnTrue()
 	}
 }
