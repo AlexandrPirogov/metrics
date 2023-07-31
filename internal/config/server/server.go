889ee64bc216af60cb7e702c903316c04545d535
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-07-31 18:49:03 +0300

    edk

--- internal/config/server/server.go
+++ internal/config/server/server.go
@@ -117,11 +117,22 @@ func initFlags() {
 	f.CompareStringsDo(hash, "", func() { ServerCfg.Hash = hash })
 	f.CompareStringsDo(storeInterval, DefaultStoreInterval, func() { JournalCfg.ReadInterval = storeInterval })
 	f.CompareStringsDo(storeFile, DefaultFileStore, func() { JournalCfg.StoreFile = storeFile })
-	f.CompareStringsDo(ServerCfg.DBUrl, DefaultDBURL, func() {
-		ServerCfg.DBUrl = dbURL
-	})
-	f.CompareStringsDoOthewise(ServerCfg.CryptoKey, DefaultCryptoKey, serverTLSAssign, serverNonTLSAssign)
 
+	if ServerCfg.DBUrl == DefaultDBURL {
+		ServerCfg.DBUrl = dbURL
+	}
+
+	if ServerCfg.CryptoKey == DefaultCryptoKey {
+		ServerCfg.Run = func(serv *http.Server) error {
+			log.Println("Running non tls server")
+			return serv.ListenAndServe()
+		}
+	} else {
+		ServerCfg.Run = func(serv *http.Server) error {
+			log.Println("Running tls server")
+			return serv.ListenAndServeTLS("server.pem", ServerCfg.CryptoKey)
+		}
+	}
 	log.Printf("server cfg from flags: %v", ServerCfg)
 	log.Printf("jounrla cfg from flasg: %v", JournalCfg)
 }
